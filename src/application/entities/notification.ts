6e91634fe59694a3ced3983dbc88e1356b4991cc
thisouzadev <thiagodesouza.dev@gmail.com>
2022-12-18 15:51:37 -0300

    getters / setters

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,37 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+  public get content(): string {
+    return this.props.content;
+  }
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+  public set readAt(readAt: Date) {
+    this.props.readAt = readAt;
+  }
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
